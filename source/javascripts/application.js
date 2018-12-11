def759bfa858173521db063423f132f415a9e25e
Kate Buxton <k8hunter@hotmail.com>
2018-12-11 12:02:26 +0000

    Copied pages over from v1 personal website

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,22 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
+
+
+$(document).on('ready', function () {
+    setTimeout(function () {
+        $('.letter').addClass('loaded');
+        $('.reg-text').addClass('loaded');
+    }, 1000);
+});
+
+
+
+
+// https://codepen.io/smashtheshell/pen/vGaLyB
+
+// var overlay = document.getElementById("overlay");
+
+// window.addEventListener('load', function(){
+//   overlay.style.display = 'none';
+// })
