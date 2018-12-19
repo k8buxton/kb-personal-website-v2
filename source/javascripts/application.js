2d2aa2365b8849b8bf1eb1579bb39f388aa16c7a
Kate Buxton <k8hunter@hotmail.com>
2018-12-19 08:53:29 +0000

    added transparent to coloured navbar

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -3,6 +3,8 @@
 //= require_tree .
 
 
+// appearing text
+
 $(document).on('ready', function () {
     setTimeout(function () {
         $('.letter').addClass('loaded');
@@ -11,12 +13,16 @@ $(document).on('ready', function () {
 });
 
 
+// transparent to coloured navbar: https://codepen.io/sonorangirl/pen/XmRBjq
 
-
-// https://codepen.io/smashtheshell/pen/vGaLyB
-
-// var overlay = document.getElementById("overlay");
-
-// window.addEventListener('load', function(){
-//   overlay.style.display = 'none';
-// })
+$(document).ready(function() {
+        // Transition effect for navbar
+        $(window).scroll(function() {
+          // checks if window is scrolled more than 20px, adds/removes solid class
+          if($(this).scrollTop() > 20) {
+              $('.navbar').addClass('solid');
+          } else {
+              $('.navbar').removeClass('solid');
+          }
+        });
+});
